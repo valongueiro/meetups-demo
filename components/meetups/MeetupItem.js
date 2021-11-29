62e6c166000a13d3d64db0dec26a4b8141f41779
valongueiro <andre.valongueiro@gmail.com>
2021-11-29 17:34:05 +0700

    done: nav bar, meetup details page, navigation

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,19 +1,27 @@
-import Card from '../ui/Card';
-import classes from './MeetupItem.module.css';
+import { useRouter } from "next/router";
+import Card from "../ui/Card";
+import classes from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
+  const { id, image, title, address } = props;
+  const router = useRouter();
+
+  function showDetailsHandler() {
+    router.push(`/${id}`);
+  }
+
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
+          <img src={image} alt={title} />
         </div>
         <div className={classes.content}>
-          <h3>{props.title}</h3>
-          <address>{props.address}</address>
+          <h3>{title}</h3>
+          <address>{address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Show Details</button>
         </div>
       </Card>
     </li>
---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Gent_Gravensteen_R01.jpg/1920px-Gent_Gravensteen_R01.jpg"
+      title="Meetup Title"
+      address="Meetup Address"
+      description="Meetup Description"
+    />
+  );
+}
+
+export default MeetupDetails;
